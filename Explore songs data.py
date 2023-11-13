398f294dcbed56b26ffae401ae3bb583ab52cf3c
Shaila Güereca <142270941+shailajgg@users.noreply.github.com>
2023-11-13 14:51:25 -0500

    Add files via upload

---
+++ Explore songs data.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %fs ls "/databricks-datasets/songs/data-001"
+
+# COMMAND ----------
+
+# MAGIC %fs head --maxBytes=10000 "/databricks-datasets/songs/README.md"
+
+# COMMAND ----------
+
+df = spark.read.format('csv').option("sep", "\t").load('dbfs:/databricks-datasets/songs/data-001/part-00000')
+df.display()
